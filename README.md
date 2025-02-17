3d288404835067f01533f15a52db6d65dad5f3a1
Chawana Maseka <chawana.maseka@gmail.com>
2025-02-17 21:03:28 +0200

    Initial commit: Basic repository structure with Python examples

---
+++ README.md
@@ -0,0 +1,16 @@
+# Python Learning Repository
+
+This repository contains Python code examples and tutorials based on Corey Schafer's Python programming series.
+
+## Structure
+- `src/`: Source code files
+- `requirements.txt`: Project dependencies
+
+## Getting Started
+1. Clone this repository
+2. Install dependencies: `pip install -r requirements.txt`
+3. Run the Python scripts from the `src` directory
+
+## Contents
+- String manipulation examples
+- String slicing operations
---
+++ src/strings.py
@@ -0,0 +1,33 @@
+#You can use both single or double quotes in python.
+#Pick one use it consistently
+
+message = "Hello World"
+print(message)
+
+message = 'Hello World'
+print(message)
+
+#Escape Character
+message = 'Bobby\'s World'
+print(message)
+
+# Multiline string
+message = """
+I love me 
+some Majimba
+"""
+print(message)
+
+# Length of string using len() function
+txt = "I love you"
+print(len(message))
+
+# Access the characters in a string
+msg = "Hello World"
+print(msg[0])
+print(msg[:5])
+print(msg[6:])
+
+
+
+#Video: https://www.youtube.com/watch?v=k9TUPpGqYTo&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&index=2&ab_channel=CoreySchafer
